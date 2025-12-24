332cef52608304aa440b19f8bd3d709a12400e91
Rodrigo Lopes Velloso <135156904+velloso99@users.noreply.github.com>
2025-12-24 18:08:33 -0300

    main

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import * #importando biblioteca tkinter
+
+
+janela = Tk() #Criando a nosa janela principal tkinter
+
+
+
+
+
+
+
+
+
+
+janela.mainloop() #Rodando a janela principal tkinter
+
